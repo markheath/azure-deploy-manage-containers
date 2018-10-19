93a1b9d4ba21a1a6c4781ffb6e09cb0fd4ad7a45
Mark Heath <mark.heath@gmail.com>
2018-10-19 20:27:50 +0100

    updated ci cd demo script

--- M6 Running Containers on Web Apps for Containers/ci-cd-appservice.ps1
+++ M6 Running Containers on Web Apps for Containers/ci-cd-appservice.ps1
@@ -8,50 +8,58 @@ $resourceGroup = "cicdappservice"
 $location = "westeurope"
 az group create -l $location -n $resourceGroup
 
-Function Get-RandomString($length)
-{
-    $validChars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWZYZ".ToCharArray()
-    Return -join ((1..$length) | ForEach-Object { $validChars | Get-Random | ForEach-Object {[char]$_} })
-}
-
-
 # create an app service plan to host
 $planName="cicdappservice"
-az appservice plan create -n $planName -g $resourceGroup -l $location --is-linux --sku S1
+az appservice plan create -n $planName -g $resourceGroup -l $location `
+                          --is-linux --sku S1
+
+
+# n.b. can't use anything but docker hub here
+# so we have to arbitrarily pick a runtime --runtime "node|6.2" or a public image like scratch
+$appName="cicd-pluralsight"
+az webapp create -n $appName -g $resourceGroup --plan $planName -i "scratch"
 
 $acrName = "pluralsightacr"
 $acrLoginServer = az acr show -n $acrName --query loginServer -o tsv
-$dockerRepo = "$acrLoginServer/samplewebapp:v2"
-# n.b. can't use anything but docker hub here
-# so we have to arbitrarily pick a runtime --runtime "node|6.2" or a public image
-$appName="cicd-$((Get-RandomString 4).ToLower())"
-az webapp create -n $appName -g $resourceGroup --plan $planName -i "scratch"
-
 $acrUserName = az acr credential show -n $acrName --query username -o tsv
 $acrPassword = az acr credential show -n $acrName --query passwords[0].value -o tsv
-# https://github.com/Azure/azure-cli/pull/3888/files - maybe don't need creds?
-az webapp config container set -n $appName -g $resourceGroup -c "$acrLoginServer/samplewebapp:v2" -r "https://$acrLoginServer" -u $acrUserName -p $acrPassword
 
-$site = az webapp show -n $appName -g $resourceGroup --query "defaultHostName" -o tsv
-Start-Process http://$site
+# https://github.com/Azure/azure-cli/pull/3888/files - maybe don't need creds?
+az webapp config container set -n $appName -g $resourceGroup `
+                               -c "$acrLoginServer/samplewebapp:latest" `
+                               -r "https://$acrLoginServer" `
+                               -u $acrUserName -p $acrPassword
+
+az webapp show -n $appName -g $resourceGroup --query "defaultHostName" -o tsv
 
 # create a staging slot (cloning from production slot's settings)
-az webapp deployment slot create -g $resourceGroup -n $appName -s staging --configuration-source $appName
+az webapp deployment slot create -g $resourceGroup -n $appName `
+                                 -s staging --configuration-source $appName
 
 az webapp show -n $appName -g $resourceGroup -s staging --query "defaultHostName" -o tsv
 
 
 # enable CD for the staging slot
-az webapp deployment container config -g $resourceGroup -n $appName -s staging --enable-cd true
+az webapp deployment container config -g $resourceGroup -n $appName `
+                                      -s staging --enable-cd true
 
 # get the webhook
-$cicdurl = az webapp deployment container show-cd-url -s staging -n $appName -g $resourceGroup --query CI_CD_URL -o tsv
+$cicdurl = az webapp deployment container show-cd-url -s staging `
+                     -n $appName -g $resourceGroup --query CI_CD_URL -o tsv
 
 # to configure the webhook on an ACR registry
-az acr webhook create --registry $acrName --name myacrwebhook01 --actions push --uri $cicdurl
+az acr webhook create --registry $acrName --name myacrwebhook --actions push `
+                      --uri $cicdurl
+
+# push a new version of our app to the ACR
+docker push pluralsightacr.azurecr.io/samplewebapp:latest
 
 # perform a slot swap
-az webapp deployment slot swap -g $resourceGroup -n $appName --slot staging --target-slot production
+az webapp deployment slot swap -g $resourceGroup -n $appName `
+                               --slot staging --target-slot production
 
 # clean up
 az group delete --name $resourceGroup --yes --no-wait
+
+# delete the webhook
+az acr webhook delete --registry $acrName --name myacrwebhook
